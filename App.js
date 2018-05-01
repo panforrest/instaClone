ea0dbddde8195ac30178a68e8e55b996c5cdba3b
panforrest <guoqianp@gmail.com>
2018-04-30 23:47:04 -0400

    45:00 render puppy using flexbox; 100+ %

--- App.js
+++ App.js
@@ -7,19 +7,7 @@ import InstaClone from './src/InstaClone.js'
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <InstaClone />
-      </View>
-    );
+    return <InstaClone />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
-});
--- src/InstaClone.js
+++ src/InstaClone.js
@@ -4,8 +4,16 @@ import { View, Text, StyleSheet, Image } from 'react-native'
 class InstaClone extends Component {
 	render(){
 		return(
-            <View>
+            <View style={{ flex: 1, width: 100 + "%", height: 100 + "%"}}>
                 <Text>My Instagram Clone</Text>
+                <Image 
+                    style={{ width: 100 + "%", height: 50}}
+		            source={{uri: 
+		              "https://lh3.googleusercontent.com/kPRNb82LdxpCTR8SaaPS3VJ4Q_tyTdAI8i6hZiwLwG-BWRQ5_v693Ipz4o3C2tAiC6N7q4MLWKBAz2g3Lc5HG6M-XA"
+		            }}
+		        />
+
+
             </View>
 		)
 	}
