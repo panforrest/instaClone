407ff56677ecea3c90e8b07599cf26f6a8745581
panforrest <guoqianp@gmail.com>
2018-05-01 00:00:10 -0400

    49:30 added a bar with background color

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -5,9 +5,16 @@ class InstaClone extends Component {
 	render(){
 		return(
             <View style={{ flex: 1, width: 100 + "%", height: 100 + "%"}}>
-                <Text>My Instagram Clone</Text>
+                <View 
+                    style={{ 
+                    	width: 100 + "%", 
+                    	height: 75, 
+                    	backgroundColor: "rgb(0,250,250)" 
+                    }}
+                />
+
                 <Image 
-                    style={{ width: 100 + "%", height: 50}}
+                    style={{ width: 100 + "%", height: 100}}
 		            source={{uri: 
 		              "https://lh3.googleusercontent.com/kPRNb82LdxpCTR8SaaPS3VJ4Q_tyTdAI8i6hZiwLwG-BWRQ5_v693Ipz4o3C2tAiC6N7q4MLWKBAz2g3Lc5HG6M-XA"
 		            }}
